8d3234cb8213734f70f97ee59856f3f979a4f1cb
Michael Banvard <mjbanvard@gmail.com>
2019-10-14 15:49:32 -0500

    Moved javascript to the .js file.

--- script.js
+++ script.js
@@ -11,3 +11,25 @@
 </ol>
 <img src="${}">
 */
+
+// Input Validation
+window.addEventListener("load", function () {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
+       let pilotName = document.querySelector("input[name=pilotName]");
+       let copilotName = document.querySelector("input[name=copilotName]");
+       let fuelLevel = document.querySelector("input[name=fuelLevel]");
+       let cargoMass = document.querySelector("input[name=cargoMass]");
+       console.log(pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value)
+       if (pilotName.value === "" || copilotName.value === "" ||
+           fuelLevel.value === "" || cargoMass.value === "") {
+           alert("All fields are required!");
+           // stop the form submission
+           event.preventDefault();
+       };
+       if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+           alert("Fuel Level and Carge Mass must be entered numerically");
+           event.preventDefault();
+       };
+   });
+});

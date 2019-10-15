8dab42b3fff6d61cf31da40808ff48451a9feba7
Michael Banvard <mjbanvard@gmail.com>
2019-10-15 11:17:46 -0500

    Moving planetary fetch in script.js.

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-{/* <h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">} */ 
-
-
 // Input Validation
 window.addEventListener("load", function () {
    let form = document.querySelector("form");
@@ -25,44 +13,29 @@ window.addEventListener("load", function () {
            fuelLevel.value === "" || cargoMass.value === "") {
            alert("All fields are required!");
            // stop the form submission
-           
        }
        if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
            alert("Fuel Level and Carge Mass must be entered numerically");
        }
        event.preventDefault();
    });
-   
+//Planetary data Fetch and HTML Display
+   fetch("https://handlers.education.launchcode.org/static/planets.json")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(planet){
+            let i= Math.floor(Math.random()*planet.length);
+            document.getElementById("missionTarget").innerHTML=  `
+                <h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: ${planet[i].name}</li>
+                    <li>Diameter: ${planet[i].diameter}</li>
+                    <li>Star: ${planet[i].star}</li>
+                    <li>Distance from Earth: ${planet[i].distance}</li>
+                    <li>Number of Moons: ${planet[i].moons}</li>
+                </ol>
+                <img src="${planet[i].image}">`
+        })
 });
 
-// window.addEventListener('load', function() {
-//    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
-//    .then(function(res){
-//        return res.json();
-//    })
-//    .then(function(json) {
-//        //console.log(json, 'Outside event.');
-//        let cont = document.getElementById("container");
-//        for (i = 0; i < json.length; i ++) {
-//            let astro = json[i];
-//            if (astro.active) {
-//                textColor = "green";
-//            } else {
-//                textColor = "black"
-//            }
-//            cont.innerHTML= cont.innerHTML + `
-//            <div class="astronaut">
-//            <div class="bio">
-//            <font color=${textColor}>
-//               <h3>${astro.firstName} ${astro.lastName}</h3>
-//               <ul>
-//                  <li>Hours in space: ${astro.hoursInSpace}</li>
-//                  <li>Active: ${astro.active}</li>
-//                  <li>Skills: ${astro.skills.join(', ')}</li>
-//               </ul>
-//            </font>
-//            </div>
-//            <img class="avatar" src=${astro.picture}>
-//            </div>`;
-//        }
-//    });

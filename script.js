af84177f1780cddfa0f1ee60323703e14fb03385
Michael Banvard <mjbanvard@gmail.com>
2019-10-15 14:37:46 -0500

    Feels like progress. lots of getElements and spans.

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
 // Write your JavaScript code here!
 
-// Input Validation
 window.addEventListener("load", function () {
    let form = document.querySelector("form");
-   form.addEventListener("submit", function (event) {
-       let pilotName = document.querySelector("input[name=pilotName]");
-       let copilotName = document.querySelector("input[name=copilotName]");
-       let fuelLevel = document.querySelector("input[name=fuelLevel]");
-       let cargoMass = document.querySelector("input[name=cargoMass]");
-       console.log(pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value)
-       if (pilotName.value === "" || copilotName.value === "" ||
-           fuelLevel.value === "" || cargoMass.value === "") {
-           alert("All fields are required!");
-           // stop the form submission
-       }
-       if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
-           alert("Fuel Level and Carge Mass must be entered numerically");
-       }
-       event.preventDefault();
-   });
-//Planetary data Fetch and HTML Display
-   fetch("https://handlers.education.launchcode.org/static/planets.json")
+   //Planetary data Fetch and HTML Display
+    fetch("https://handlers.education.launchcode.org/static/planets.json")
         .then(function(response){
             return response.json();
         })
@@ -37,5 +20,54 @@ window.addEventListener("load", function () {
                 </ol>
                 <img src="${planet[i].image}">`
         })
+    // Input Validation
+    form.addEventListener("submit", function (event) {
+       let pilotName = document.querySelector("input[name=pilotName]");
+       let copilotName = document.querySelector("input[name=copilotName]");
+       let fuelLevel = document.querySelector("input[name=fuelLevel]");
+       let cargoMass = document.querySelector("input[name=cargoMass]");
+       console.log(pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value)
+       if (pilotName.value === "" || copilotName.value === "" ||
+           fuelLevel.value === "" || cargoMass.value === "") {
+           alert("All fields are required!");
+       }
+       if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+           alert("Fuel Level and Carge Mass must be entered numerically");
+       }
+       event.preventDefault();
+    // stop the form submission
+       
+    //If shuttle is not ready to launch
+
+//     <div id="launchStatusCheck">
+//     <h2 id="launchStatus">Awaiting Information Before Launch</h2>
+//     <div id="faultyItems">
+//         <ol>
+//             <li id="pilotStatus">Pilot Ready</li>
+//             <li id="copilotStatus">Co-pilot Ready</li>
+//             <li id="fuelStatus">Fuel level high enough for launch</li>
+//             <li id="cargoStatus">Cargo mass low enough for launch</li>
+//         </ol>
+//      </div>
+//      </div>
+
+    document.getElementById("launchStatus").innerHTML= 
+        //<h1 style="color:blue;">This is a Blue Heading</h1>
+        `<p style="color:red;">Shuttle not ready to Launch</p>`
+    // let newItem = 
+    //     `<style>
+    //         li {visibility:visible;}
+    //     </style>`
+    // document.getElementById("faultyItems").insertBefore(newItem, `<ol>`)
+        
+    document.getElementById("pilotStatus").innerHTML=
+        `<span style="visibility:visible">Pilot ${pilotName.value} is ready for launch</span>`
+    document.getElementById("copilotStatus").innerHTML=
+        `<span style="visibility:visible">Co-Pilot ${copilotName.value} is ready for launch</span>`
+    document.getElementById("fuelStatus").innerHTML=
+        `<span style="visibility:visible">Fuel level too low for launch</span>`
+       });
+
+    
 });
 

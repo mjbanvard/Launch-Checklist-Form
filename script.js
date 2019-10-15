fa402176594215ef2f3373f7e965747d811ff7ca
Michael Banvard <mjbanvard@gmail.com>
2019-10-14 19:22:50 -0500

    Workshop Monday. Terence Help. Yay!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Write your JavaScript code here!
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
+{/* <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${}</li>
    <li>Diameter: ${}</li>
@@ -9,8 +9,8 @@
    <li>Distance from Earth: ${}</li>
    <li>Number of Moons: ${}</li>
 </ol>
-<img src="${}">
-*/
+<img src="${}">} */ 
+
 
 // Input Validation
 window.addEventListener("load", function () {
@@ -25,11 +25,44 @@ window.addEventListener("load", function () {
            fuelLevel.value === "" || cargoMass.value === "") {
            alert("All fields are required!");
            // stop the form submission
-           event.preventDefault();
-       };
+           
+       }
        if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
            alert("Fuel Level and Carge Mass must be entered numerically");
-           event.preventDefault();
-       };
+       }
+       event.preventDefault();
    });
+   
 });
+
+// window.addEventListener('load', function() {
+//    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+//    .then(function(res){
+//        return res.json();
+//    })
+//    .then(function(json) {
+//        //console.log(json, 'Outside event.');
+//        let cont = document.getElementById("container");
+//        for (i = 0; i < json.length; i ++) {
+//            let astro = json[i];
+//            if (astro.active) {
+//                textColor = "green";
+//            } else {
+//                textColor = "black"
+//            }
+//            cont.innerHTML= cont.innerHTML + `
+//            <div class="astronaut">
+//            <div class="bio">
+//            <font color=${textColor}>
+//               <h3>${astro.firstName} ${astro.lastName}</h3>
+//               <ul>
+//                  <li>Hours in space: ${astro.hoursInSpace}</li>
+//                  <li>Active: ${astro.active}</li>
+//                  <li>Skills: ${astro.skills.join(', ')}</li>
+//               </ul>
+//            </font>
+//            </div>
+//            <img class="avatar" src=${astro.picture}>
+//            </div>`;
+//        }
+//    });
